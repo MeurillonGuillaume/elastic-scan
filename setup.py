a89c948b231be19a4ea74bfe76786973d88bc669
Guillaume Meurillon <guillaume.meurillon@be-mobile.com>
2020-05-26 13:42:31 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='elastic_scan',
     py_modules=['elastic_scan'],
-    version='0.2.1',
+    version='0.2.2',
     author='Guillaume Meurillon',
     install_requires=[
         'elasticsearch',
