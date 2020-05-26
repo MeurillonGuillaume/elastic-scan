07656840dcc26582f4f3405171f371dd85abba00
Guillaume Meurillon <guillaume.meurillon@be-mobile.com>
2020-05-26 16:03:41 +0200

    code updates

--- elastic_scan.py
+++ elastic_scan.py
@@ -21,9 +21,7 @@ def __get_npartitions(doc_count, scroll_size):
     :returns: The required amount of partitions
     :rtype: int
     """
-    _res = (doc_count // scroll_size) + 1
-    logging.info(f'Scan will create {_res} partitions')
-    return _res
+    return (doc_count // scroll_size) + 1
 
 
 def __elastic_scanner(index, client, auth=('', ''), query={}, scroll_size=10000, timeout='1m', responsetype='hits'):
@@ -33,8 +31,11 @@ def __elastic_scanner(index, client, auth=('', ''), query={}, scroll_size=10000,
     :param index: An index to scan
     :type index: str
 
-    :param client_address: A list of addresses or a string
-    :type client_address: list, str
+    :param client: A list of addresses or a string
+    :type client: list, str
+
+    :param auth: A tuple in the form of (USERNAME, PASSWORD)
+    :type auth: tuple
 
     :param query: A query to execute for filtering
     :type query: dict
@@ -56,16 +57,33 @@ def __elastic_scanner(index, client, auth=('', ''), query={}, scroll_size=10000,
             try:
                 yield hit['_source']
             except Exception as e:
-                logging.error(e)
+                logging.error(f'Failed to yield scroll _source: {e}')
     elif responsetype == AGGREGATION:
         try:
             yield {'index': index, 'aggregation': _es.search(index=index, size=scroll_size, scroll=timeout, body=query)['aggregations']}
         except Exception as e:
-            logging.error(e)
+            logging.error(f'Failed to yield aggregation response: {e}')
 
 
-def __get_doc_count(index, client, auth, query):
-    return Elasticsearch(hosts=client, http_auth=auth).search(index=index, body=query, size=0)['hits']['total']
+def __get_doc_count(index, client, auth=('', ''), query={}):
+    """
+    Request the amount of documents that will be retrieved
+
+    :param index: An index to scan
+    :type index: str
+
+    :param client: A list of addresses or a string
+    :type client: list, str
+
+    :param query: A query to execute for filtering
+    :type query: dict
+
+    :param auth: A tuple in the form of (USERNAME, PASSWORD)
+    :type auth: tuple
+
+    :rtype: int
+    """
+    return int(Elasticsearch(hosts=client, http_auth=auth).search(index=index, body=query, size=0)['hits']['total'])
 
 
 def __scan(index, client, auth=('', ''), query={}, scroll_size=10000, timeout='1m', responsetype=HITS):
@@ -75,11 +93,14 @@ def __scan(index, client, auth=('', ''), query={}, scroll_size=10000, timeout='1
     :param index: An index to scan
     :type index: str
 
-    :param client_address: A list of addresses or a string
-    :type client_address: list, str
+    :param client: A list of addresses or a string
+    :type client: list, str
 
     :param query: A query to execute for filtering
     :type query: dict
+
+    :param auth: A tuple in the form of (USERNAME, PASSWORD)
+    :type auth: tuple
 
     :param scroll_size: Maximum amount of docs returned per iteration
     :type scroll_size: int
@@ -115,50 +136,6 @@ def __add_slice(query, sliceid, slicemax):
     return query
 
 
-def __get_shard_info(index, client, auth=('', '')):
-    """
-    Request info about the shards used for an index or alias
-
-    :param index: The name of the index or alias
-    :type index: str
-
-    :param client: The endpoint to request info from
-    :type client: str, list
-
-    :rtype: list
-    :returns: A list of endpoints with information required to query a primary shard of an index
-    """
-    return [
-        {
-            'shardID': int(shard['shard']),
-            'docCount': int(shard['docs']),
-            'endpoint': shard['ip'],
-            'index': shard['index']
-        } for shard in Elasticsearch(hosts=client, http_auth=auth).cat.shards(index=index, format='json') if shard['prirep'] == 'p' and int(shard['docs']) > 0]
-
-
-def __get_hostname_ips(client_addresses):
-    _result = {}
-    if isinstance(client_addresses, list):
-        for _address in client_addresses:
-            _address = str(_address)
-            _key = _address
-
-            # Replace preposition
-            for _filter in ['https', 'http', '://']:
-                _address = _address.replace(_filter, '')
-
-            # Replace eventual sockets
-            if len(_address.split(':')) > 1:
-                for _tmp in _address.split(':'):
-                    if len(_tmp) > 1:
-                        _address = _tmp
-                        break
-
-            _result[socket.gethostbyname(_address)] = _key
-    return _result
-
-
 def scan_index(index, client, auth=('', ''), query={}, scroll_size=10000, n_partitions=None, timeout='1m', responsetype=HITS):
     """
     Scan a complete index
@@ -183,9 +160,7 @@ def scan_index(index, client, auth=('', ''), query={}, scroll_size=10000, n_part
 
     :rtype: delayed
     """
-
     _dataholder = []
-    _hostname_ips = __get_hostname_ips(client_addresses=client)
     _doc_count = __get_doc_count(
         index=index, client=client, auth=auth, query=query)
 
